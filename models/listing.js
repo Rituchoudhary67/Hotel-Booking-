9df8866db393ed5ecf9b6b5261658fd09b23d0b8
Ritu Choudhary <114747083+Rituchoudhary67@users.noreply.github.com>
2025-06-05 00:03:07 +0530

    listing Schema added

--- models/listing.js
+++ models/listing.js
@@ -8,9 +8,11 @@ const listingSchema = new Schema({
     },
     description: String,
     image: {
-        type: String,
-        default: "https://media.istockphoto.com/id/1205289672/photo/majestic-bright-sunrise-over-ocean.jpg?s=1024x1024&w=is&k=20&c=Iu120uS9o3MkPOOVK7dmZAZqugnaFEgO8Tgzcm41Y8k=",
-        set: (v) => v === "" ? "https://media.istockphoto.com/id/1205289672/photo/majestic-bright-sunrise-over-ocean.jpg?s=1024x1024&w=is&k=20&c=Iu120uS9o3MkPOOVK7dmZAZqugnaFEgO8Tgzcm41Y8k=" : v,
+        // type: String,
+        // default: "https://media.istockphoto.com/id/1205289672/photo/majestic-bright-sunrise-over-ocean.jpg?s=1024x1024&w=is&k=20&c=Iu120uS9o3MkPOOVK7dmZAZqugnaFEgO8Tgzcm41Y8k=",
+        // set: (v) => v === "" ? "https://media.istockphoto.com/id/1205289672/photo/majestic-bright-sunrise-over-ocean.jpg?s=1024x1024&w=is&k=20&c=Iu120uS9o3MkPOOVK7dmZAZqugnaFEgO8Tgzcm41Y8k=" : v,
+        filename: String,
+        url: String
     },
     price: Number,
     location: String,
