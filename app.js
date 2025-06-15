2b25fc3c5e3e468ff08c86a38bbff22648cee4ee
Ritu Choudhary <114747083+Rituchoudhary67@users.noreply.github.com>
2025-06-15 20:08:12 +0530

    delete route added

--- app.js
+++ app.js
@@ -75,6 +75,14 @@ app.put("/listings/:id", async (req,res) => {
     res.redirect(`/listings/${id}`);
 });
 
+//Delete route
+app.delete("/listings/:id", async (req,res) => {
+    let {id} = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+})
+
 
 // app.get("/testListing", async (req,res) => {
 //     let sampleListing = new Listing({
